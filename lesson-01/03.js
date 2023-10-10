eb1b543ea8daf45ef33dfdb3a8834a0f48f78f36
Eugene <bysoundtrack@gmail.com>
2023-10-10 16:12:12 +0300

    l1t3

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -13,7 +13,7 @@
 // console.log(number**);
 // console.log(**number);
 let number = 1;
-console.log(number--);
+console.log(number++);
 console.log(++number);
 console.log(number--);
 console.log(--number);
