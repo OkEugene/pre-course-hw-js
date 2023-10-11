afe5e51c359bcd2f914240b446d88bc989c96c59
Eugene <bysoundtrack@gmail.com>
2023-10-11 10:26:36 +0300

    l1t6

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,4 +1,5 @@
 // Создай программу для let str = "zzzzyzz"; используя тернарный оператор и метод includes. Если нашел букву "y", то отконсоль "нашел", если не нашел, то"заблудился"
 
 let str = "zzzzyzz";
-let message = str.includes("y") ? "нашел" : "заблудился";
+let result = str.includes("y") ? "нашел" : "заблудился";
+console.log(result);
