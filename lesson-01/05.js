7a22680a5dffc6477d097765b18d7a77daebeef9
Eugene <bysoundtrack@gmail.com>
2023-10-16 10:42:35 +0300

    l1t5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -11,14 +11,14 @@
 // default:...
 // }
 
-let rule = "Еще не родился тот человек, который поставил бы цель.";
+let rule = "Еще не родился тот человек, который поставил бы цель."; ћ
 switch (true) {
   case rule.length < 25:
     console.log("Все таки нет правил без исключения");
     break;
-  case rule.length === 25:
-    console.log("50 на 50");
+  case rule.length > 25:
+    console.log("У меня 100 пудов все получится");
     break;
   default:
-    console.log("У меня 100 пудов все получится");
+    console.log("50 на 50");
 }
